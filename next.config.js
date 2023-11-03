02d2e6bae583d75bf8d69dc2ff219cea7082d61f
FernandoNLopez <lopezfer1998@outlook.com>
2023-11-03 15:02:07 -0300

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "guzliqnaajflswiomxnq.supabase.co"
+    ]
+  }
+}
 
 module.exports = nextConfig
